9cf3df48c537ba5ee5467f688c3611185af97bd2
Berkant Ay <berkantay@192.168.1.103>
2022-01-04 00:54:27 +0300

    listed

--- src/app.js
+++ src/app.js
@@ -10,47 +10,19 @@ axios
     // products variable below by destructuring
     // You can use alias
     const products = response.data;
-    let product_names = [];
-    let product_links = [];
 
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
-    products.forEach((element) => {
-      product_names.push(element.description);
-    });
 
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
-    const match = product_names.filter((item) => {
-      return item.includes("Şal");
+    const match = products.filter((item) => {
+      return item.description.includes("Şal");
     });
     console.log(match);
 
-    match.forEach((element) => {
-      for (let index = 0; index < product_names.length; index++) {
-        if (element === products[index].description) {
-          product_links.push(products[index].image);
-        }
-      }
-    });
-
-    const map = new Map();
-
-    match.forEach((element, index) => {
-      // console.log(element.description);
-      map.set(element, product_links[index]);
-    });
-    const mappedProducts = map;
-    console.log(mappedProducts);
-
-    product_links = [];
-    product_names = [];
-
-    console.log(Array.from(mappedProducts.keys())[0]);
-
-    appendData(mappedProducts);
-    // appendData(product_json);
+    appendData(match);
 
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
@@ -62,15 +34,14 @@ axios
 
 function appendData(data) {
   var mainContainer = document.getElementById("some-page-wrapper");
-  for (var i = 0; i < data.size; i++) {
+  for (var i = 0; i < data.length; i++) {
     var div_description = document.createElement("div");
     div_description.className = "description-area";
-    var key = Array.from(data.keys())[i];
-    div_description.innerHTML = "Name: " + Array.from(data.keys())[i];
+    div_description.innerHTML = "Name: " + data[i].description;
     mainContainer.appendChild(div_description);
     var div_image = document.createElement("img");
     div_image.className = "image-area";
-    div_image.src = data.get(key);
+    div_image.src = data[i].image;
     mainContainer.appendChild(div_image);
   }
 }
