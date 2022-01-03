02ed59c3c90c7c11dcda5cf85c2433a1c9dec952
Berkant Ay <berkantay@192.168.1.103>
2022-01-03 22:40:47 +0300

    desired map object created ready for frontend

--- src/app.js
+++ src/app.js
@@ -5,38 +5,48 @@ axios
   .then((response) => {
     // Firstly, log response to the console,
     // inspect the response and see that it has data field
-    console.log(response)
+    console.log(response);
     // Assign data field of the response to
     // products variable below by destructuring
     // You can use alias
     const products = response.data;
     const product_names = [];
-    
-   
+    const product_links = [];
+
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
-    products.forEach((element, index) => {
-      console.log(element.description);
+    products.forEach((element) => {
+      // console.log(element.description);
       product_names.push(element.description);
     });
 
-
-    const match = product_names.filter(item => {
-      return item.includes('Şal');
+    const match = product_names.filter((item) => {
+      return item.includes("Şal");
     });
     console.log(match);
 
-    const temp_map = new Map();
-    
-    match.forEach((element,index)=>{
-      temp_map.set(element,)
-    })
+    match.forEach((element) => {
+      for (let index = 0; index < product_names.length; index++) {
+        if (element === products[index].description) {
+          product_links.push(products[index].image);
+        }
+      }
+    });
+
+    console.log(product_links);
 
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
-    const mappedProducts = null;
-  
+
+    const map = new Map();
+
+    match.forEach((element, index) => {
+      // console.log(element.description);
+      map.set(element, product_links[index]);
+    });
+    const mappedProducts = map;
+
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
     // you need to use forEach method
