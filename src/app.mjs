79914fb5d51076486f68fad989b7ff5eac515d6e
berkantay <berkant.ay@siemens.com>
2022-01-03 06:08:42 -0800

    done until mapping

--- src/app.mjs
+++ src/app.mjs
@@ -1,3 +1,4 @@
+import "./styles.css";
 import axios from "axios";
 
 axios
@@ -5,33 +6,20 @@ axios
   .then((response) => {
     // Firstly, log response to the console,
     // inspect the response and see that it has data field
-    console.log(response)
+    console.log
     // Assign data field of the response to
     // products variable below by destructuring
     // You can use alias
-    const products = response.data;
-    const product_names = [];
-    
-   
+    const products = null;
+
     // Print names of all product to the console
     // by calling foreach  method (use arrow function)
-    products.forEach((element, index) => {
-      console.log(element.description);
-      product_names.push(element.description);
-    });
 
-
-    const match = product_names.filter(item => {
-      return item.includes('Şal');
-    });
-    console.log(match);
-
-    const temp_map = new Map();
     // Get all products that contain "Şal" in their name (use filter method)
     // map filtered products to new object having only image and name field
     // assign mapped items to mappedProducts variable
     const mappedProducts = null;
-  
+
     // Display the images and names of mappedProducts
     // You need to add them to the DOM
     // you need to use forEach method
